a03a8a1be8f175b630c5d834f32c9f4768d17db7
Alexander Gnusarev <alexander.gnussarev@gmail.com>
2024-08-23 22:40:22 +0600

    fix test

--- tests/test_clustering_model.py
+++ tests/test_clustering_model.py
@@ -6,6 +6,5 @@ from ml_api.cluster_model import clustering_data, read_data
 def test_clustering_data() -> None:
     _path = Path(Path(__file__).resolve().parent / "data/german_credit_data_testing.csv")
     _data = read_data(_path)
-    _prediction_data = _data.drop("Cluster", axis=1)
-    _prediction_data = clustering_data(_prediction_data)
+    _prediction_data = clustering_data(_data)
     assert_frame_equal(_data, _prediction_data)
